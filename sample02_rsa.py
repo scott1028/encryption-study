5aa5e2a8129e2a2f7c725db69bd7f1947e292541
scott.lan <scott.lan@taisys.com>
2017-05-25 13:13:23 +0800

    update code

--- sample02_rsa.py
+++ sample02_rsa.py
@@ -8,13 +8,13 @@ from Crypto.Cipher import PKCS1_OAEP
 
 # 偽隨機數生成器
 random_generator = Random.new().read
-# rsa算法生成實例, 如果要實作 Wik 上的演算法要靠這邊取得 'n', 'e', 'd', 'p', 'q', 'u' 否則等產生 Private & Public Key 再取會缺少部份數值.
+# rsa算法生成實例, 如果要實作 Wik 上的演算法要靠這邊取得 n, e, d, p, q, u 否則等產生 Private & Public Key 再取會缺少部份數值.
 rsa = RSA.generate(1024, random_generator)
 
 # master的秘鑰對的生成
-private_pem = rsa.exportKey()
+private_pem = rsa.exportKey()  # 通常將完整包含 n, e, d, p, q, u
 
-public_pem = rsa.publickey().exportKey()
+public_pem = rsa.publickey().exportKey()  # 大多外面實作的 Library As Public Key 只會輸出 n, e
 
 print '-' * 40
 
@@ -56,11 +56,22 @@ def decrypt(c, d, n):
 	return m
 
 MSG2 = 123
-# 由先前的 RSA.generate(1024, random_generator) 取得 'n', 'e', 'd', 'p', 'q', 'u' 值
-# 一般造成 RSA Lib Public Key 只能加 與 Private Key 只能解 就是於 Export Key 的時候把 'n', 'e', 'd', 'p', 'q', 'u' 分別包入相對缺少特定值則只能加不能解！
+# 由先前的 RSA.generate(1024, random_generator) 取得 n, e, d, p, q, u 值
+# 一般造成 RSA Lib Public Key 只能加 與 Private Key 只能解 就是於 Export Key 的時候把 n, e, d, p, q, u 分別包入相對缺少特定值則只能加不能解！
+# 
+# 相對 Key 為
+# A-Key = (n, e) -> (rsa.n, rsa.e)
+# B-Key = (n, d) -> (rsa.n, rsa.e)
+# 
 # Formula: c = (m ** e) % n
 # Formula: m = (c ** d) % n
 
 c = encrypt(MSG2, rsa.e, rsa.n)
 m = decrypt(c, rsa.d, rsa.n)
 print MSG2 == m, MSG2, m
+
+c1 = encrypt(MSG2, rsa.d, rsa.n)
+m1 = decrypt(c1, rsa.e, rsa.n)
+print MSG2 == m1, MSG2, m1
+
+import pdb; pdb.set_trace()
