a13c99802d4d82f399ed8ca179fb62e78ed9bb3b
scott.lan <scott.lan@taisys.com>
2017-05-25 13:07:12 +0800

    add more sample

--- sample02_rsa.py
+++ sample02_rsa.py
@@ -57,8 +57,10 @@ def decrypt(c, d, n):
 
 MSG2 = 123
 # 由先前的 RSA.generate(1024, random_generator) 取得 'n', 'e', 'd', 'p', 'q', 'u' 值
+# 一般造成 RSA Lib Public Key 只能加 與 Private Key 只能解 就是於 Export Key 的時候把 'n', 'e', 'd', 'p', 'q', 'u' 分別包入相對缺少特定值則只能加不能解！
 # Formula: c = (m ** e) % n
 # Formula: m = (c ** d) % n
+
 c = encrypt(MSG2, rsa.e, rsa.n)
 m = decrypt(c, rsa.d, rsa.n)
 print MSG2 == m, MSG2, m
