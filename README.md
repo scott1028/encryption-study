9a471a8806f48d6ad874411c5ebcf3a70ea10b9f
scott.lan <scott.lan@taisys.com>
2017-05-25 12:02:01 +0800

    update code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # 非對稱式加密運作方式
 
 - Ref: https://zh.wikipedia.org/wiki/RSA%E5%8A%A0%E5%AF%86%E6%BC%94%E7%AE%97%E6%B3%95
+- Ref: https://pythonhosted.org/pycrypto/
 
 ```
 Encrypt formula: c = (n^e) mod n
@@ -8,7 +9,10 @@ Decript formula: c^d = ( n^(e-d) ) mod n
 ```
 
 ```
-A-Key encrypts message to chipher -> B-Key decrypts chipher to message.
+A-Key encrypts message to cipher -> B-Key decrypts cipher to message.
 	... or
-B-Key encrypts message to chipher -> A-Key decrypts chipher to message.
+B-Key encrypts message to cipher -> A-Key decrypts cipher to message.
+P.S 但大部份實作 Private Key 不會作為加密使用!
 ```
+
+- ![Alt text](https://raw.githubusercontent.com/scott1028/master/sample02_rsa.png "sample02_rsa.png")
---
+++ sample02_rsa.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+import base64
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+
+# 偽隨機數生成器
+random_generator = Random.new().read
+# rsa算法生成實例
+rsa = RSA.generate(1024, random_generator)
+
+# master的秘鑰對的生成
+private_pem = rsa.exportKey()
+
+public_pem = rsa.publickey().exportKey()
+
+print '-' * 40
+
+print private_pem
+print
+print public_pem
+
+print '-' * 40
+
+# CONST
+MSG = 'tested by scott'
+
+# encryt by public_pem key testcase
+key1 = RSA.importKey(public_pem)
+cipher = PKCS1_OAEP.new(key1)
+cipher_txt = cipher.encrypt(MSG)
+print base64.b64encode(cipher_txt)
+
+# decrypt by private_pem key testcase
+key2 = RSA.importKey(private_pem)
+cipher = PKCS1_OAEP.new(key2)
+msg = cipher.decrypt(cipher_txt)
+print msg
